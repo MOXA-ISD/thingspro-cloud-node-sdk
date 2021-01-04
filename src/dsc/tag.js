bff8fe513cf03e6bc4bc3f222a17816e6411e866
Justin Liu <justintwd@gmail.com>
2021-01-04 16:37:49 +0800

    fix: fix get tag  failed

--- src/dsc/tag.js
+++ src/dsc/tag.js
@@ -29,8 +29,6 @@ module.exports = gv => {
     let $r = await request.get(
       `/projects/${projectId}/tags`
     )
-    console.log($r.status)
-    console.log(JSON.stringify($r.data))
     if ($r.data.data.length) {
       const tag = $r.data.data.find(tag => tag.tagName === tagName)
       if (tag) { return tag }
